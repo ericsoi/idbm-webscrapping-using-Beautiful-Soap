66338ed6faab28a9a1dc0fd7e7e8fb7dcc954b44
root <root@DESKTOP-FUPLPL4.localdomain>
2019-09-27 19:28:29 +0300

    Readme modified

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # idbm-webscrapping-using-Beautiful-Soap
+A python script that gets movie name, cast members and certificates from idbm movie website.
+Tested on ubuntu 18.04.
+
+Prerequisites:
+	- Install python3
+		$ apt install python3
+	- Install pip3
+		$ apt install python3-pip
+	- Install BeautifulSoup using pip
+		$ pip3 install bs4
+
+How to:
+- Get get the script from github
+	$ git clone https://github.com/ericsoi/idbm-webscrapping-using-Beautiful-Soap.git
+- Run the scripts
+	$ chmod u+x scrap.py
+	$ ./scrap.py
+
+You can customize the script to get more custom information. 
+
--- scrap.py
+++ scrap.py
@@ -8,8 +8,57 @@ import csv
 movielinklist = []	
 page = requests.get('https://www.imdb.com/list/ls005750764/?sort=list_order,asc&st_dt=&mode=detail&page=6')
 soup = BeautifulSoup(page.content, 'html.parser')
-whatamlookingfor = soup.find
-(class_="lister list detail sub-list")
+whatamlookingfor = soup.find(class_="lister list detail sub-list")
 movies = whatamlookingfor.find_all(class_="lister-item mode-detail")
 with open('output.csv', 'w') as file:
 	collumns = ['moviename', 'cast', 'certificates']
+	writer = csv.DictWriter(file, fieldnames=collumns)
+	writer.writeheader()
+	for i, movie in enumerate(movies):
+		link = movie.find(class_="lister-item-image ribbonize")
+		movielinks = link.find('a')['href']
+		moviename = link.find('img')['alt']
+		movielink = 'https://www.imdb.com{}fullcredits'.format(movielinks.split('?')[0])
+		certificates = 'https://www.imdb.com{}parentalguide'.format(movielinks.split('?')[0])
+		movielinklist.append(movielink)
+		writer.writerow({'moviename' : moviename})
+		print('\n')
+		print('Moviename:', i, moviename)
+		print("----------------------------------------------")
+		print(moviename + '\'s cast members')
+		print('----------------------------------------------')
+		page = requests.get(movielink)
+		soup = BeautifulSoup(page.content, 'html.parser')
+		castlist = soup.find(class_="cast_list")
+		mylist = list(castlist.children)[3:-1]
+		#for i in mylist:
+		#	try:
+		#		castmembers = i.find('img')['alt']
+		#		writer.writerow({'cast' : castmembers})
+		#		print(castmembers)
+		#	except TypeError:
+		#		pass
+		#print('----------------------------------------------')
+		#print(moviename + '\'s certificates')
+		#print('----------------------------------------------')
+		certpage = requests.get(certificates)
+		certsoup = BeautifulSoup(certpage.content, 'html.parser')
+		certificationlist = certsoup.find(id='certifications-list')
+		certifications = certificationlist.find_all(class_='ipl-inline-list__item')
+		#for certification in certifications:
+		#	certification = (certification.get_text().replace('\n', ''))
+		#	certification = re.sub(' +',' ', certification)
+		#	print(certification)
+		#	writer.writerow({'certificates' : certification})
+		print('--------------------------------------------------------------------------------------------------------------')
+		for i, certification in zip(mylist, certifications):
+			certification = (certification.get_text().replace('\n', ''))
+			certification = re.sub(' +',' ', certification)
+			try:
+				castmembers = i.find('img')['alt']
+				writer.writerow({'moviename' : moviename, 'cast' : castmembers, 'certificates' : certification})
+				print(moviename)
+				print(castmembers)
+				print(certification)
+			except TypeError:
+				pass
